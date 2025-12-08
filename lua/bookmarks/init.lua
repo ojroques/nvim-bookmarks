6267ad205e6e628f17c7fd53ac794e3b504b049a
Olivier Roques <oroques@fastmail.com>
2025-12-08 09:25:46 +0100

    feat: rename global module from M to Bookmarks

--- lua/bookmarks/init.lua
+++ lua/bookmarks/init.lua
@@ -3,7 +3,7 @@ File = require('bookmarks.file')
 Namespace = require('bookmarks.namespace')
 Shada = require('bookmarks.shada')
 Window = require('bookmarks.window')
-M = {}
+Bookmarks = {}
 
 local defaults = {
   augroup = 'Bookmarks',
@@ -11,105 +11,105 @@ local defaults = {
   shada_dir = string.format('%s/%s', vim.fn.stdpath('data'), 'bookmarks'),
 }
 
-function M.add()
+function Bookmarks.add()
   local path = vim.api.nvim_buf_get_name(0)
   local pos = vim.api.nvim_win_get_cursor(0)
   local f = File:new(path, pos)
-  M.shada:add(M.namespace, f)
+  Bookmarks.shada:add(Bookmarks.namespace, f)
 end
 
-function M.update()
+function Bookmarks.update()
   local path = vim.api.nvim_buf_get_name(0)
   local pos = vim.api.nvim_win_get_cursor(0)
   local f = File:new(path, pos)
-  M.shada:update(M.namespace, f)
+  Bookmarks.shada:update(Bookmarks.namespace, f)
 end
 
-function M.remove()
+function Bookmarks.remove()
   local path = vim.api.nvim_buf_get_name(0)
   local f = File:new(path, nil)
-  M.shada:remove(M.namespace, f)
+  Bookmarks.shada:remove(Bookmarks.namespace, f)
 end
 
-function M.open(index)
-  local file = M.shada:file(M.namespace, index)
+function Bookmarks.open(index)
+  local file = Bookmarks.shada:file(Bookmarks.namespace, index)
   if file ~= nil then
     file:edit()
   end
 end
 
-function M.open_next()
+function Bookmarks.open_next()
   local path = vim.api.nvim_buf_get_name(0)
   local f = File:new(path, nil)
-  local i, _ = M.shada:get(M.namespace, f)
+  local i, _ = Bookmarks.shada:get(Bookmarks.namespace, f)
   if i == nil then
-    M.open(1)
+    Bookmarks.open(1)
   else
-    M.open(i + 1)
+    Bookmarks.open(i + 1)
   end
 end
 
-function M.open_previous()
+function Bookmarks.open_previous()
   local path = vim.api.nvim_buf_get_name(0)
   local f = File:new(path, nil)
-  local i, _ = M.shada:get(M.namespace, f)
+  local i, _ = Bookmarks.shada:get(Bookmarks.namespace, f)
   if i == nil then
-    M.open(M.shada:length(M.namespace))
+    Bookmarks.open(Bookmarks.shada:length(Bookmarks.namespace))
   else
-    M.open(i - 1)
+    Bookmarks.open(i - 1)
   end
 end
 
-function M.reset(files)
-  M.shada:purge(M.namespace)
+function Bookmarks.reset(files)
+  Bookmarks.shada:purge(Bookmarks.namespace)
 
   if files == nil then
     return
   end
 
   for _, f in ipairs(files) do
-    M.shada:add(M.namespace, f)
+    Bookmarks.shada:add(Bookmarks.namespace, f)
   end
 end
 
-function M.toggle_menu()
-  if M.bookmark_manager ~= nil then
-    M.bookmark_manager:close()
+function Bookmarks.toggle_menu()
+  if Bookmarks.bookmark_manager ~= nil then
+    Bookmarks.bookmark_manager:close()
   end
 
   local callbacks = {
     on_select = function(file) if file ~= nil then file:edit() end end,
-    on_write = function(files) M.reset(files) end,
-    on_close = function() M.bookmark_manager = nil end,
+    on_write = function(files) Bookmarks.reset(files) end,
+    on_close = function() Bookmarks.bookmark_manager = nil end,
   }
 
-  M.bookmark_manager = Buffer:new(M.shada:files(M.namespace), defaults.augroup, callbacks)
+  Bookmarks.bookmark_manager = Buffer:new(Bookmarks.shada:files(Bookmarks.namespace), defaults.augroup, callbacks)
 
   local path = vim.api.nvim_buf_get_name(0)
-  local row, _ = M.shada:get(M.namespace, File:new(path, nil))
+  local row, _ = Bookmarks.shada:get(Bookmarks.namespace, File:new(path, nil))
 
-  Window:new(M.bookmark_manager.buf, M.cwd, M.namespace, row)
+  Window:new(Bookmarks.bookmark_manager.buf, Bookmarks.cwd, Bookmarks.namespace, row)
 end
 
 local function on_vim_leave(_)
-  if M.shada ~= nil then
-    M.shada:save()
+  if Bookmarks.shada ~= nil then
+    Bookmarks.shada:save()
   end
 end
 
 local function on_buf_leave(_)
-  M.namespace = Namespace.git_branch(vim.fn.getcwd()) or defaults.namespace
-  M.update()
+  Bookmarks.namespace = Namespace.git_branch(vim.fn.getcwd()) or defaults.namespace
+  Bookmarks.update()
 end
 
 local function on_dir_changed(args)
-  if M.shada ~= nil then
-    M.shada:save()
+  if Bookmarks.shada ~= nil then
+    Bookmarks.shada:save()
   end
 
-  M.cwd = args.file
-  M.namespace = Namespace.git_branch(M.cwd) or defaults.namespace
-  M.shada = Shada:new(defaults.shada_dir, M.cwd)
+  Bookmarks.cwd = args.file
+  Bookmarks.namespace = Namespace.git_branch(Bookmarks.cwd) or defaults.namespace
+  Bookmarks.shada = Shada:new(defaults.shada_dir, Bookmarks.cwd)
 end
 
 local function create_autocommands()
@@ -119,9 +119,9 @@ local function create_autocommands()
   vim.api.nvim_create_autocmd('DirChangedPre', {group = group, callback = on_dir_changed})
 end
 
-function M.setup()
+function Bookmarks.setup()
   on_dir_changed({file = vim.fn.getcwd()})
   create_autocommands()
 end
 
-return M
+return Bookmarks
